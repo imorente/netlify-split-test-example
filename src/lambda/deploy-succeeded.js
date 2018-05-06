43c1e280c745a8799b89ca1f2ab47acc54d9a1f6
Irene Morente <imorente@gmail.com>
2018-05-06 16:05:25 -0700

    Add test deploy_succeeded webhook

--- src/lambda/deploy-succeeded.js
+++ src/lambda/deploy-succeeded.js
@@ -1,7 +1,37 @@
-exports.handler = async (event, context) => {
-  console.log("deploy succeeded", { event, context });
-  return {
-    statusCode: 200,
-    body: "Hello, Lambda!"
-  };
+import fetch from "node-fetch";
+
+const SLACK_HOOK =
+  "https://hooks.slack.com/services/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";
+
+exports.handler = (event, context, callback) => {
+  const { payload: deploy, site } = JSON.parse(event.body);
+  const { branch, published_at } = deploy;
+  try {
+    fetch(SLACK_HOOK, {
+      headers: {
+        "content-type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+        text: JSON.stringify({
+          branch,
+          published_at
+        })
+      })
+    })
+      .then(response => {
+        console.log({ response });
+        callback(null, {
+          statusCode: response.status,
+          body: response.statusText
+        });
+      })
+      .catch(error => {
+        console.log({ error });
+        callback(null, { statusCode: 200, body: "error" });
+      });
+  } catch (error) {
+    console.log(error);
+    callback(null, { statusCode: 200, body: "error" });
+  }
 };
