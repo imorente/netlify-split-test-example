e8954873ff8e67f56a71a8557023b9fec2eb4253
Irene Morente <imorente@gmail.com>
2018-05-07 16:43:48 -0700

    Add Slack webhook as an environment variable

--- src/lambda/deploy-succeeded.js
+++ src/lambda/deploy-succeeded.js
@@ -1,37 +1,22 @@
 import fetch from "node-fetch";
 
-const SLACK_HOOK =
-  "https://hooks.slack.com/services/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";
+const SLACK_HOOK = process.env.SLACK_WEBHOOK_URL;
 
 exports.handler = (event, context, callback) => {
   const { payload: deploy, site } = JSON.parse(event.body);
   const { branch, published_at } = deploy;
-  try {
-    fetch(SLACK_HOOK, {
-      headers: {
-        "content-type": "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify({
-        text: JSON.stringify({
-          branch,
-          published_at
-        })
-      })
+  fetch(SLACK_HOOK, {
+    headers: {
+      "content-type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({
+      text: `branch: ${branch}, published_at: ${published_at}`
     })
-      .then(response => {
-        console.log({ response });
-        callback(null, {
-          statusCode: response.status,
-          body: response.statusText
-        });
-      })
-      .catch(error => {
-        console.log({ error });
-        callback(null, { statusCode: 200, body: "error" });
-      });
-  } catch (error) {
-    console.log(error);
-    callback(null, { statusCode: 200, body: "error" });
-  }
+  }).then(response =>
+    callback(null, {
+      statusCode: response.status,
+      body: response.statusText
+    })
+  );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
